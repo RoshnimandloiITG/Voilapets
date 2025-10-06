19bfab6522b0fda016352b652faf7919c263a12d
Roshni <roshni@itgeeks.com>
2025-10-06 18:40:55 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -7,24 +7,28 @@ const sidebarCrossBtn = document.getElementById('sidebarCrossBtn');
 const hamburg = document.getElementById('hamburg');
 const onCLickLinks = document.getElementById("onCLickLinks");
 const cross = document.getElementById('cross');
+
 // event listner to toggel search bar 
 searchBar.addEventListener('click', () => {
-    searchBarPopUp.style.display = "block";
+    // searchBarPopUp.style.display = "block";
+    searchBarPopUp.style.visibility = "visible";
     document.body.classList.add('hidden');
 })
 crossBtn.addEventListener('click', () => {
-    searchBarPopUp.style.display = "none";
+    searchBarPopUp.style.visibility = "hidden";
+    // searchBarPopUp.style.display = "none";
     document.body.classList.remove('hidden');
 })
 dustbinBar.addEventListener('click', () => {
-    sideBar.style.display = "block";
+    // sideBar.style.display = "block";
+    sideBar.style.visibility = "visible";
     document.body.classList.add('hidden');
-
 })
 sidebarCrossBtn.addEventListener('click', () => {
-    sideBar.style.display = "none";
+    // sideBar.style.display = "none";
+    sideBar.style.visibility = "hidden";
     document.body.classList.remove('hidden');
-
+    console.log("hello");
 })
 hamburg.addEventListener('click', () => {
     onCLickLinks.style.display = "block";
